8b6bf57b54b78bb8e383b53c80aae0e2a2de8eca
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-03-17 01:40:35 +0100

    startBackEnd

---
+++ email.php
@@ -0,0 +1,12 @@
+<?php
+
+function email($email, $name, $company, $message)
+{
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $company = $_POST["company"];
+    $message = $_POST["message"];
+
+}
+
+?>
