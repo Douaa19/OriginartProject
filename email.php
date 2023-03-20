75f0fb38cf616eee9aca46e0f578eca0d557d054
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-03-20 22:54:23 +0000

    phpMailer

--- email.php
+++ email.php
@@ -1,19 +1,65 @@
 <?php
+ob_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require "./includes/PHPMailer-master/src/Exception.php";
+require "./includes/PHPMailer-master/src/PHPMailer.php";
+require "./includes/PHPMailer-master/src/SMTP.php";
+
+$error = "";
 
 if (isset($_POST["submit"])) {
-    $name = $_POST["name"];
-    $company = $_POST["company"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    if (empty($_POST["name"]) || empty($_POST["company"]) || empty($_POST["email"]) || empty($_POST["subject"]) || empty($_POST["message"])) {
+        $error = "You have to complete the form!";
+        header("Location: index.php?error=${error}");
+    } else {
+        $name = $_POST["name"];
+        $company = $_POST["company"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
 
 
-    $mailTo = "douaa.larif@outlook.fr";
-    $headers = "From: " . $email;
-    $txt = "You have to received an e-mail from " . $name . ".\n\n" . $message;
+        $mailTo = "douaa.larif@outlook.fr";
+        $headers = "From: " . $email;
+        $txt = "You have received an e-mail from " . $name . ".\n" . $message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: back.php?mailsend");
+
+        $mail = new PHPMailer(true);
+
+        try {
+            // Set the mail server settings
+            $mail->isSMTP();
+            $mail->SMTPDebug = 2;
+            $mail->SMTPAuth = true;
+            $mail->Host = "tls://smtp.gmail.com";
+            $mail->SMTPKeepAlive = true;
+            $mail->Mailer = "smtp";
+            $mail->SMTPAuth = true;
+            $mail->Username = "originart.contact@gmail.com";
+            $mail->Password = "eczbokmtkuswkglq";
+            $mail->SMTPSecure = "tls";
+            $mail->Port = "587";
+            $mail->CharSet = "UTF-8";
+
+            // Set the recipients for the mail
+            $mail->setFrom($email, $name);
+            $mail->addAddress("originart.contact@gmail.com", "Originart");
+
+            // Set the content of the mail
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->msgHTML($txt);
+
+            // Sending the mail
+            $mail->send();
+            header("Location: back.php?mailsend");
+        } catch (Exception $e) {
+            echo "Message could not be send: {$mail->ErrorInfo}";
+        }
+    }
 }
-
-
+ob_end_flush();
 ?>
--- index.php
+++ index.php
@@ -1,90 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./style/header.css" />
-    <link rel="stylesheet" href="./style/contact-us.css" />
-    <link rel="stylesheet" href="./style/footer.css" />
-    <title>Document</title>
-  </head>
-  <body>
-     <header class="container">
-      <nav>
-        <img src="./images/Or logo-white.svg" alt="">
-        <div class="link">
-          <span>Contact</span>
-        </div>
-      </nav>
-      <!-- <div class="hero-header">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./style/header.css" />
+  <link rel="stylesheet" href="./style/contact-us.css" />
+  <link rel="stylesheet" href="./style/footer.css" />
+  <title>Document</title>
+</head>
+
+<body>
+  <header class="container">
+    <nav>
+      <img src="./images/Or logo-white.svg" alt="">
+      <div class="link">
+        <span>Contact</span>
+      </div>
+    </nav>
+    <!-- <div class="hero-header">
         <div class="scrolling_img">
           <img class="img1" src="./images/33-02.jpg" alt="" /> -->
-          <!-- <img class="img2" src="./images/A4 hikayat.jpg" alt="" /> -->
-          <!-- <img
+    <!-- <img class="img2" src="./images/A4 hikayat.jpg" alt="" /> -->
+    <!-- <img
             class="img3"
             src="./images/Affiche A4 blfan Final-02.jpg"
             alt=""
           /> -->
-          <!-- <img class="img4" src="./images/affiche B_LFEN 2.jpg" alt="" /> -->
-          <!-- <img class="img5" src="./images/affiche f.jpg" alt="" />
+    <!-- <img class="img4" src="./images/affiche B_LFEN 2.jpg" alt="" /> -->
+    <!-- <img class="img5" src="./images/affiche f.jpg" alt="" />
           <img class="img6" src="./images/AFFICHE.jpg" alt="" /> -->
-        </div>
-      </div>
-    </header>
-    <section class="contact-us">
-      <div class="container">
-        <div class="form">
-          <h1>contact us</h1>
-          <form action="./email.php" method="post">
-            <div class="name">
-              <input type="text" name="name" placeholder="name" />
-            </div>
-            <div class="company">
-              <input type="text" name="company" placeholder="company" />
-            </div>
-            <div class="email">
-              <input type="email" name="email" placeholder="email" />
-            </div>
-            <div class="message">
-              <textarea
-                name="message"
-                id="message"
-                cols="35"
-                rows="5"
-                placeholder="message"
-              ></textarea>
-            </div>
-            <div class="btn">
-                <button type="submit" name="submit" class="custom-btn btn-5">submit</button>
-            </div>
-          </form>
-        </div>
-        <div class="follow-us">
-          <h1>follow us</h1>
-          <div class="icons">
-            <a href="/"><img src="./images/icons/icons8-facebook.svg" alt=""></a>
-            <a href="/"><img src="./images/icons/icons8-instagram-vieux-logo.svg" alt=""></a>
-            <a href="/"><img src="./images/icons/icons8-linkedin-2.svg" alt=""></a>
-            <a href="/"><img src="./images/icons/icons8-youtube-encadré.svg" alt=""></a>
+    </div>
+    </div>
+  </header>
+  <section class="contact-us">
+    <div class="container">
+      <div class="form">
+        <h1>contact us</h1>
+        <form action="./email.php" method="post">
+          <div class="error-message">
+            <?php
+            if (isset($_GET["error"])) {
+              echo $_GET["error"];
+            }
+            ?>
           </div>
-        </div>
-      </div>
-    </section>
-    <footer>
-      <div class="container">
-        <div class="part-one">
-          <img src="./images/Originart logo-white.svg" alt="">
-          <p>L'originalité, c'est ce qui nous libère de la routine</p>
-        </div>
-        <div class="part-two">
-          <img src="./images/Or logo-white.svg" alt="">
-          <div class="copy-right">
-            <span>©&nbsp</span>
-            <span>2023</span>
+          <div class="name">
+            <input type="text" name="name" placeholder="name" />
           </div>
+          <div class="company">
+            <input type="text" name="company" placeholder="company" />
+          </div>
+          <div class="email">
+            <input type="email" name="email" placeholder="email" />
+          </div>
+          <div class="subject">
+            <input type="text" name="subject" placeholder="subject" />
+          </div>
+          <div class="message">
+            <textarea name="message" id="message" cols="35" rows="5" placeholder="message"></textarea>
+          </div>
+          <div class="btn">
+            <button type="submit" name="submit" class="custom-btn btn-5">submit</button>
+          </div>
+        </form>
+      </div>
+      <div class="follow-us">
+        <h1>follow us</h1>
+        <div class="icons">
+          <a href="/"><img src="./images/icons/icons8-facebook.svg" alt=""></a>
+          <a href="/"><img src="./images/icons/icons8-instagram-vieux-logo.svg" alt=""></a>
+          <a href="/"><img src="./images/icons/icons8-linkedin-2.svg" alt=""></a>
+          <a href="/"><img src="./images/icons/icons8-youtube-encadré.svg" alt=""></a>
         </div>
       </div>
-    </footer>
-  </body>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="part-one">
+        <img src="./images/Originart logo-white.svg" alt="">
+        <p>L'originalité, c'est ce qui nous libère de la routine</p>
+      </div>
+      <div class="part-two">
+        <img src="./images/Or logo-white.svg" alt="">
+        <div class="copy-right">
+          <span>©&nbsp</span>
+          <span>2023</span>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
 </html>
