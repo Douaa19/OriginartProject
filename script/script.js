23576c6c9262331835e111849e51102aebd96f47
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-04-05 03:22:04 +0000

    setStyleForDarkAndLightMode

--- script/script.js
+++ script/script.js
@@ -1,17 +1,5 @@
-(function () {
-  var quotes = $(".word");
-  var quoteIndex = -1;
-
-  function showNextQuote() {
-    ++quoteIndex;
-    quotes
-      .eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
-  }
-
-  showNextQuote();
-})();
-
-console.assert("hello animation");
+document.querySelector(".theme-toggle-button").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.getElementById("moon").style.display = "none";
+  document.getElementById("sun").style.display = "block";
+});
